f153445878ace80c13f9c3a11915c1aeacaf13cb
Tushar K <tushars6016@gmail.com>
2022-10-14 00:37:54 +0530

    Update eks-worker-nodes.tf

--- eks-worker-nodes.tf
+++ eks-worker-nodes.tf
@@ -39,7 +39,7 @@ resource "aws_iam_role_policy_attachment" "demo-node-AmazonEC2ContainerRegistryR
 }
 
 resource "aws_eks_node_group" "eks-1" {
-  cluster_name    = aws_eks_cluster.demo.name
+  cluster_name    = aws_eks_cluster.eks-cluster.name
   node_group_name = "eks-1"
   node_role_arn   = aws_iam_role.eks-node.arn
   subnet_ids      = aws_subnet.eks-sub[*].id
